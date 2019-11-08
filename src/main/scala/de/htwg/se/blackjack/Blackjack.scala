38d5c98cf1dd390ddc3f58309d8a3d901200539b
Daniel <bersuchdaniel@yahoo.de>
2019-11-08 10:00:37 +0100

    Split two Names in one Array with two values

--- src/main/scala/de/htwg/se/blackjack/Blackjack.scala
+++ src/main/scala/de/htwg/se/blackjack/Blackjack.scala
@@ -2,12 +2,26 @@ package de.htwg.se.blackjack
 
 import de.htwg.se.blackjack.model.{Card, Player}
 
+
+
 object  Blackjack {
   def main(args: Array[String]): Unit = {
-    val a = scala.io.StdIn.readLine()
-    val b = scala.io.StdIn.readLine()
-    val daniel = Player(a)
-    val marc = Player(b)
-    println("Hi wir sind " + daniel + " und " + marc + " und wir Programmieren Blackjack in Scala !")
+    def menu(): Unit = {
+      println("Willkommen zu Blackjack!")
+      println("<Name1>_<Name2>: Beide Spieler werden ausgegeben")
+      println("q: Programm beenden.")
+    }
+    menu()
+    val input = scala.io.StdIn.readLine()
+
+
+    if(input.equals("q")) {
+      println("Bye!")
+    } else {
+      val split = input.split(" ")
+      val daniel = Player(split(0))
+      val marc = Player(split(1))
+        println("Hi wir sind " + daniel + " und " + marc + " und wir Programmieren Blackjack in Scala !")
+    }
   }
 }
