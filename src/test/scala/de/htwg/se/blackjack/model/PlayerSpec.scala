a5e9ea44401920d576484027dcc43029fee9aa46
Daniel <bersuchdaniel@yahoo.de>
2019-11-21 12:39:13 +0100

    Test improvements

--- src/test/scala/de/htwg/se/blackjack/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/blackjack/model/PlayerSpec.scala
@@ -2,21 +2,25 @@ package de.htwg.se.blackjack.model
 
 import org.scalatest._
 
+import scala.collection.immutable
+
 class PlayerSpec extends WordSpec with Matchers {
   "A Player" when { "new" should {
-    val daniel = Player("Daniel")
-    val marc = Player("Marc")
+    val daniel = Player("Daniel", Nil)
+    val marc = Player("Marc", Nil)
     "have a name" in {
       daniel.name should be("Daniel")
+      daniel.cards should be(Nil)
     }
     "have also a name" in {
       marc.name should be("Marc")
+      marc.cards should be(Nil)
     }
     "have a nice String representation" in {
-      daniel.toString should be ("Daniel")
+      daniel.toString should be (daniel.name + "'s cards: " + daniel.cards)
     }
     "have also a nice String representation" in {
-      marc.toString should be ("Marc")
+      marc.toString should be (marc.name + "'s cards: " + marc.cards)
     }
   }}
 }
