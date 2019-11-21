60e9d5ee84c8d11fa34c613fce43cdb69ecf52c7
Daniel <bersuchdaniel@yahoo.de>
2019-11-21 12:38:33 +0100

    Add List[Card] Parameter

--- src/main/scala/de/htwg/se/blackjack/model/Player.scala
+++ src/main/scala/de/htwg/se/blackjack/model/Player.scala
@@ -1,5 +1,14 @@
 package de.htwg.se.blackjack.model
 
-case class Player(name: String) {
-  override def toString:String = name;
+case class Player(name: String, cards: List[Card]) {
+  def takeCard(card: Option[Card]): Player = {
+    card match {
+      case Some(x) => this.copy(cards = x :: cards)
+      case None => this
+    }
+  }
+
+  override def toString:String ={
+    name + "'s cards: " + cards
+  }
 }
