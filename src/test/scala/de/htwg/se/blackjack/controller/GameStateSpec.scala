f3dbaf931278369d7ba150d15640b6fbe826eac1
Daniel <bersuchdaniel@yahoo.de>
2020-01-23 22:32:05 +0100

    Add Test for GameState

---
+++ src/test/scala/de/htwg/se/blackjack/controller/GameStateSpec.scala
@@ -0,0 +1,26 @@
+package de.htwg.se.blackjack.controller
+
+import org.scalatest.{Matchers, WordSpec}
+
+class GameStateSpec extends WordSpec with Matchers {
+  " A GameState " when {
+    " new " should {
+      val won = GameState.WON
+      val lost = GameState.LOST.id
+      val bust = GameState.BUST.id
+      val push = GameState.PUSH.id
+    }
+    "Won" in {
+       GameState.WON.id should be(1)
+    }
+    "Lost" in {
+      GameState.LOST.id should be(-1)
+    }
+    "Bust" in {
+      GameState.BUST.id should be(-1)
+    }
+    "Push" in {
+      GameState.PUSH.id should be(0)
+    }
+  }
+}
