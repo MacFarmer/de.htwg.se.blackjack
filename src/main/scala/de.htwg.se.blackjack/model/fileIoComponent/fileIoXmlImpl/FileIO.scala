f14556d381c223edfe282aa7be76d85948bd14a0
Daniel <bersuchdaniel@yahoo.de>
2020-01-23 23:29:43 +0100

    fixed test again

--- src/main/scala/de.htwg.se.blackjack/model/fileIoComponent/fileIoXmlImpl/FileIO.scala
+++ src/main/scala/de.htwg.se.blackjack/model/fileIoComponent/fileIoXmlImpl/FileIO.scala
@@ -6,7 +6,6 @@ import com.google.inject.Guice
 import de.htwg.se.blackjack.BlackjackModule
 import de.htwg.se.blackjack.model.cardComponent.cardBaseImpl.CardFactory
 import de.htwg.se.blackjack.model.fileIoComponent.FileIOInterface
-import de.htwg.se.blackjack.model.handComponent.HandInterface
 import de.htwg.se.blackjack.model.playerComponent.PlayerInterface
 import de.htwg.se.blackjack.model.playerComponent.playerBaseImpl.Player
 import de.htwg.se.blackjack.model.statusComponent.StatusInterface
